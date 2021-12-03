dce92df74a2694b826e5d32ad43be552d81835ce
Steven <dynm@outlook.com>
2021-12-03 15:40:13 +0800

    add probe tracer

--- apetracer/keccaktracer.go
+++ apetracer/keccaktracer.go
@@ -22,7 +22,30 @@ type Tracer interface {
 }
 */
 
-type KeccakTracer struct{}
+type KeccakOp struct {
+	Contract common.Address
+	Preimage []byte
+	Hash     common.Hash
+}
+
+type StorageOp struct {
+	Contract common.Address
+	Key      common.Hash
+	Value    common.Hash
+	IsWrite  bool
+}
+
+func NewKeccakTracer() *KeccakTracer {
+	return &KeccakTracer{
+		keccakOps:  make([]KeccakOp, 0),
+		storageOps: make([]StorageOp, 0),
+	}
+}
+
+type KeccakTracer struct {
+	keccakOps  []KeccakOp
+	storageOps []StorageOp
+}
 
 func (tracer *KeccakTracer) CaptureStart(env *vm.EVM, from common.Address, to common.Address, create bool, input []byte, gas uint64, value *big.Int) {
 }
@@ -33,6 +56,7 @@ type keccakState interface {
 }
 
 func (tracer *KeccakTracer) CaptureState(env *vm.EVM, pc uint64, op vm.OpCode, gas, cost uint64, scope *vm.ScopeContext, rData []byte, depth int, err error) {
+	contract := scope.Contract.Address()
 	switch op {
 	case vm.SHA3:
 		{
@@ -53,15 +77,53 @@ func (tracer *KeccakTracer) CaptureState(env *vm.EVM, pc uint64, op vm.OpCode, g
 			// if evm.Config.EnablePreimageRecording {
 			// 	evm.StateDB.AddPreimage(interpreter.hasherBuf, data)
 			// }
-			contract := scope.Contract.Address()
-			callData := scope.Contract.Input
-
-			log.Printf("Keccak Detected\nContract: %s\ncalldata: 0x%02x\npreimage: %02x\nhash: %s", contract.Hex(), callData, data, hasherBuf.Hex())
+			// callData := scope.Contract.Input
+			keccakOp := KeccakOp{
+				Contract: contract,
+				Preimage: data,
+				Hash:     hasherBuf,
+			}
+			tracer.keccakOps = append(tracer.keccakOps, keccakOp)
+			// log.Printf("Keccak Detected\nContract: %s\ncalldata: 0x%02x\npreimage: %02x\nhash: %s", contract.Hex(), callData, data, hasherBuf.Hex())
 		}
 	case vm.ORIGIN:
 		{
-			contract := scope.Contract.Address()
 			log.Printf("Contract: %s uses tx.origin", contract.Hex())
+		}
+	case vm.REVERT:
+		{
+			stack := scope.Stack.Data()
+			offset, size := stack[len(stack)-1], stack[len(stack)-2]
+			data := scope.Memory.GetPtr(int64(offset.Uint64()), int64(size.Uint64()))
+			log.Printf("Contract: %s revert: 0x%02x(%s)", contract.Hex(), data, string(data))
+		}
+	case vm.SLOAD:
+		{
+			stack := scope.Stack.Data()
+			key := stack[len(stack)-1]
+			keyHash := common.Hash(key.Bytes32())
+			val := env.StateDB.GetState(scope.Contract.Address(), keyHash)
+			storageOp := StorageOp{
+				Contract: contract,
+				Key:      keyHash,
+				Value:    val,
+				IsWrite:  false,
+			}
+			tracer.storageOps = append(tracer.storageOps, storageOp)
+		}
+	case vm.SSTORE:
+		{
+			stack := scope.Stack.Data()
+			key, value := stack[len(stack)-1], stack[len(stack)-2]
+			keyHash := common.Hash(key.Bytes32())
+			valHash := common.Hash(value.Bytes32())
+			storageOp := StorageOp{
+				Contract: contract,
+				Key:      keyHash,
+				Value:    valHash,
+				IsWrite:  true,
+			}
+			tracer.storageOps = append(tracer.storageOps, storageOp)
 		}
 	}
 }
@@ -71,3 +133,12 @@ func (tracer *KeccakTracer) CaptureExit(output []byte, gasUsed uint64, err error
 func (tracer *KeccakTracer) CaptureFault(env *vm.EVM, pc uint64, op vm.OpCode, gas, cost uint64, scope *vm.ScopeContext, depth int, err error) {
 }
 func (tracer *KeccakTracer) CaptureEnd(output []byte, gasUsed uint64, t time.Duration, err error) {}
+
+func (tracer *KeccakTracer) GetResult() ([]KeccakOp, []StorageOp) {
+	return tracer.keccakOps, tracer.storageOps
+}
+
+func (tracer *KeccakTracer) Reset() {
+	tracer.keccakOps = make([]KeccakOp, 0)
+	tracer.storageOps = make([]StorageOp, 0)
+}
