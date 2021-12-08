1c2703b164ae82a0a61591dcb7a7c04ba961a34a
Steven <dynm@outlook.com>
2021-12-08 19:30:20 +0800

    update make getData promising

--- ape-safer-extension/src/utils.js
+++ ape-safer-extension/src/utils.js
@@ -7,14 +7,22 @@ export function docall(cmd, params) {
     params: params,
   };
 
-  var rpcAddress = "http://127.0.0.1:10545/";
+  // https://stackoverflow.com/questions/37700051/chrome-extension-is-there-any-way-to-make-chrome-storage-local-get-return-so
+  function getData(sKey) {
+    return new Promise(function (resolve, reject) {
+      chrome.storage.local.get(sKey, function (items) {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError.message);
+          reject(chrome.runtime.lastError.message);
+        } else {
+          resolve(items[sKey]);
+        }
+      });
+    });
+  }
 
-  chrome.storage.local.get(["apesafer-rpc"], (items) => {
-    if (items["apesafer-rpc"] !== undefined) {
-      rpcAddress = items["apesafer-rpc"];
-    }
-
-    var ret = fetch(rpcAddress, {
+  return getData(["apesafer-rpc"]).then(function (rpcURL) {
+    var ret = fetch(rpcURL, {
       headers: {
         accept: "*/*",
         "content-type": "application/json",
