21e1aa05738bcb810f51469e6ec4aba48a24bf84
Steven <dynm@outlook.com>
2021-12-10 12:43:45 +0800

    add fullTrace

--- ape-safer-extension/src/TraceView.js
+++ ape-safer-extension/src/TraceView.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import { docall } from "./utils.js";
-// import SyntaxHighlighter from "react-syntax-highlighter";
-// import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import TextField from "@mui/material/TextField";
 import ReactJson from "react-json-view";
 
 function TraceView() {
-  const [trace, setTrace] = useState({});
+  const [callTrace, setCallTrace] = useState({});
+  const [fullTrace, setFullTrace] = useState({});
   const searchParams = new URLSearchParams(window.location.search);
   const txhash = searchParams.get("txhash");
   console.log(txhash);
@@ -22,22 +22,35 @@ function TraceView() {
               traceLog.address !== "0xa9e5afe700000000a9e5afe700000000a9e5afe7"
             ) {
               console.log("trace not found");
-              setTrace({ err: "Trace not found" });
+              setCallTrace({ err: "Trace not found" });
             } else {
               const traceJSON = Buffer.from(
                 traceLog.data.replace("0x", ""),
                 "hex"
               ).toString();
-              const traceJSONStr = JSON.stringify(
-                JSON.parse(traceJSON),
-                null,
-                2
-              );
-              setTrace(JSON.parse(traceJSON));
-              // setTrace("aa");
+              setCallTrace(JSON.parse(traceJSON));
             }
           } else {
-            setTrace({ err: "Trace not found" });
+            setCallTrace({ err: "Trace not found" });
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
+
+  useEffect(() => {
+    docall("debug_traceTransaction", [txhash])
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          if (result.hasOwnProperty("result")) {
+            const traceResult = result.result;
+            const fullTraceStr = JSON.stringify(traceResult, null, 2);
+            setFullTrace(fullTraceStr);
+          } else {
+            setFullTrace(JSON.stringify({ err: "Trace not found" }));
           }
         },
         (error) => {
@@ -48,7 +61,18 @@ function TraceView() {
 
   return (
     <div style={{ textAlign: "left" }}>
-      <ReactJson src={trace} displayDataTypes={false} enableClipboard={false} />
+      <ReactJson
+        src={callTrace}
+        displayDataTypes={false}
+        enableClipboard={false}
+      />
+      <TextField
+        style={{ textAlign: "left" }}
+        value={fullTrace}
+        multiline
+        rows={500}
+        fullWidth
+      />
     </div>
   );
 }
