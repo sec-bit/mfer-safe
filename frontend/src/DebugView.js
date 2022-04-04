349124d61588fafe969053581a9a1b2470640558
Steven <dynm@outlook.com>
2022-04-04 23:15:01 +0800

    fix warnings

--- frontend/src/DebugView.js
+++ frontend/src/DebugView.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import List from "@mui/material/List";
+// import List from "@mui/material/List";
 import TextField from "@mui/material/TextField";
 
 import ListItem from "@mui/material/ListItem";
@@ -63,7 +63,7 @@ export default function DebugView() {
           console.log(error);
         }
       );
-  }, []);
+  }, [traceID,txhash]);
   return (
     <Box
       sx={{
