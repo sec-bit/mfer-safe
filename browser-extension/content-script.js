54c54d365e18582d8d9e863c347b8514569310c4
Steven <dynm@outlook.com>
2022-07-19 23:10:27 +0800

    add chrome extension

---
+++ browser-extension/content-script.js
@@ -0,0 +1,32 @@
+var s = document.createElement("script");
+s.src = chrome.runtime.getURL("script.js");
+s.onload = function () {
+  this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
+
+window.addEventListener(
+  "message",
+  (event) => {
+    // We only accept messages from ourselves
+    if (event.source != window) {
+      return;
+    }
+
+    if (event.data.type && event.data.type == "FROM_PAGE") {
+      var request = event.data.request;
+      chrome.runtime.sendMessage(request, (response) => {
+        window.postMessage(
+          {
+            type: "TO_PAGE",
+            text: "hello from content script",
+            request: request,
+            response: response,
+          },
+          "*"
+        );
+      });
+    }
+  },
+  false
+);
