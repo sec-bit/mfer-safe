0ea329df83954bab262fb461b8062e0ed5bdff72
Steven <dynm@outlook.com>
2022-08-25 15:44:42 +0800

    fix persisit log view

--- mfer-safe-desktop-app/src/App.js
+++ mfer-safe-desktop-app/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import * as React from "react";
+import {useState, useEffect} from "react"
 
 import NavTabs from "./NavTabs";
 import SimulateView from "./SimulateView";
@@ -8,9 +9,22 @@ import SettingsView from "./SettingsView";
 import TxDataOverview from "./TxDataOverview";
 import TraceView from "./TraceView";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { listen } from "@tauri-apps/api/event";
 
 function App() {
-  const [log,setLog] = React.useState("")
+  const [log, setLog] = useState("")
+  useEffect(() => {
+    console.log("init tauri event listener")
+    listen("mfernode-event", (event) => {
+      if (event.payload !== undefined) {
+        setLog((log) => {
+          var logLines = log.split("\n").slice(0, 500);
+          log = logLines.join("\n");
+          return event.payload + "\n" + log
+        });
+      }
+    });
+  }, []);
   return (
     <Router>
       <div>
@@ -20,7 +34,7 @@ function App() {
           <Route path="/txs" element={<TxDataOverview />} />
           <Route path="/trace/:txHash" element={<TraceView />} />
           <Route path="/safemultisend" element={<SimulateView />} />
-          <Route path="/logs" element={<LogsView setLog={setLog} log={log} />} />
+          <Route path="/logs" element={<LogsView log={log} />} />
         </Routes>
       </div>
     </Router>
--- mfer-safe-desktop-app/src/LogsView.js
+++ mfer-safe-desktop-app/src/LogsView.js
@@ -1,21 +1,8 @@
 import * as React from "react";
-import { useEffect } from "react";
 import TextareaAutosize from "@mui/material/TextareaAutosize";
-import { listen } from "@tauri-apps/api/event";
 
 export default function LogView(props) {
-  const { log, setLog } = props;
-  useEffect(() => {
-    listen("mfernode-event", (event) => {
-      if (event.payload !== undefined) {
-        setLog((log) => {
-          var logLines = log.split("\n").slice(0, 500);
-          log = logLines.join("\n");
-          return event.payload + "\n" + log
-        });
-      }
-    });
-  }, []);
+  const { log } = props;
   return (
     <TextareaAutosize
       aria-label="empty textarea"
